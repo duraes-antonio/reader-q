006f69b8ac75b77617e66cbfec8f4e87321b75f1
duraes-antonio <garotoseis@gmail.com>
2022-02-27 22:02:40 -0300

    CLEAN: Remove unused import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,4 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BottomTabs } from './src/main/navigation/bottom-tabs';
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
     return <BottomTabs />;
